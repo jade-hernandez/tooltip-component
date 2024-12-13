0f5fa928ccc7044b60a8b277abbaa2ec76fbfd21
Jade Hernandez <jade.hernandez88@gmail.com>
2024-12-13 14:34:59 +0100

    Init starter project

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+
+import "./globals.css";
+
+const noto = Noto_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Title",
+  description: "Description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={noto.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="mx-auto min-h-screen pt-[200px] bg-gradient-to-b from-[#F9FAFB] to-[#D2D6DB]">
+      <h1>Hello World</h1>
+    </main>
+  );
+}
