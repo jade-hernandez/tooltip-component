67374f8165dbc14f762382a62da053f3a7c616f4
Jade Hernandez <jade.hernandez88@gmail.com>
2024-12-18 16:34:45 +0100

    Update Tailwind CSS configuration to add custom screen size and improve formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,16 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      colors: {},
-    },
+      screens: {
+        lg: "1440px"
+        // => @media (min-width: 1440px) { ... }
+      },
+      colors: {}
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
